b9eade279d6aedf51b6e042c79cf57d8e640aa66
ArtyomZaycev <artem.zaycev.234@gmail.com>
2023-12-16 02:45:24 +0300

    Add memory usage report

---
+++ src/api/other.rs
@@ -0,0 +1,34 @@
+
+pub mod types {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct UserMemoryUsageData {
+        pub events_count: usize,
+        pub passwords_count: usize,
+        pub schedules_count: usize,
+        pub event_plans_count: usize,
+        pub event_templates_count: usize,
+        pub bytes: usize,
+    }
+}
+
+// ADMIN ONLY REQUEST
+pub mod load_user_memory_usage {
+    use super::types::*;
+    use http::Method;
+    use serde::{Deserialize, Serialize};
+
+    pub static METHOD: Method = Method::GET;
+    pub static PATH: &str = "load_user_memory_usage";
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Args {
+        pub user_id: i32,
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Response {
+        pub data: UserMemoryUsageData,
+    }
+}
